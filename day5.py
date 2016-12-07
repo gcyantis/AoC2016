6f43f5b0c9a3b2ead6a2e5197b09e472e4ef8445
gcyantis <colin@gcyantis.com>
2016-12-07 14:42:20 -0500

    Change Day 5 output
    Changed output for day 5 to update the same line for each character
    found rather than printing a new line

--- day5.py
+++ day5.py
@@ -13,20 +13,21 @@ def part1():
 		if hash[0:5] == '00000':
 			password += hash[5]
 		i += 1
-		
+
 	print(password)
 
 def part2():
-	door = 'ffykfhsq'
+	door = 'abc'
 	i = 1
 	password = ['_','_','_','_','_','_','_','_',]
-
+	print("Generating Password: "+''.join(password), end="\r")
 	while '_' in password:
 		hash = md5hash(door+str(i))
 		if hash[0:5] == '00000':
 			pos = hash[5]
 			if pos.isnumeric() and int(pos) < 8 and password[int(pos)] == '_':
 				password[int(pos)] = hash[6]
-				print(''.join(password))
+				print("Generating Password: "+''.join(password), end="\r")
 		i += 1
+	print("     Found Password: "+''.join(password))
 part2()
